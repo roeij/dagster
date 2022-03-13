761776e75dad835b9d73b2639aa6b20e5bcccf85
Sean Mackesey <s.mackesey@gmail.com>
2022-03-12 23:57:55 -0800

    AliasedModuleFinder

--- python_modules/dagster/dagster_tests/general_tests/test_api.py
+++ python_modules/dagster/dagster_tests/general_tests/test_api.py
@@ -14,7 +14,7 @@ def test_all():
         if (
             not exported.startswith("_")
             and not inspect.ismodule(getattr(dagster, exported))
-            and not exported in dagster._DEPRECATED
+            and not exported in dagster._DEPRECATED  # pylint: disable=protected-access
         ):
             assert exported in dagster.__all__
 
@@ -23,3 +23,13 @@ def test_deprecated_imports():
     with pytest.warns(DeprecationWarning, match=re.escape('"EventMetadataEntry" is deprecated')):
         from dagster import EventMetadataEntry, MetadataEntry
     assert EventMetadataEntry is MetadataEntry
+
+
+def test_deprecated_submodule_import_paths():
+    import dagster._core
+    import dagster._core.definitions
+    import dagster.core  # type: ignore
+    import dagster.core.definitions  # type: ignore
+
+    assert dagster.core == dagster._core  # pylint: disable=protected-access
+    assert dagster.core.definitions == dagster._core.definitions  # pylint: disable=protected-access
