b31349c60e02a3a76e0e39524aa98664002c4d98
Claire Lin <claire@elementl.com>
2022-04-01 11:05:44 -0700

    add check for no run ids

--- js_modules/dagit/packages/core/src/workspace/asset-graph/FailedRunsSinceMaterializationBanner.tsx
+++ js_modules/dagit/packages/core/src/workspace/asset-graph/FailedRunsSinceMaterializationBanner.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 import {titleForRun} from '../../runs/RunUtils';
-import {runsPathWithFilters} from '../../runs/RunsFilterInput';
 
 import {LiveDataForNode, __ASSET_GROUP} from './Utils';
 
